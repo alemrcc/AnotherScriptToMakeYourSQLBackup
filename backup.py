be4ecf1c9c64e01038b9c912251cf7bafe71b936
Alessandro <72794815+alemrcc@users.noreply.github.com>
2023-11-06 14:04:24 +0100

    new file:   __pycache__/const.cpython-39.pyc 	modified:   backup.py 	modified:   const.py

--- backup.py
+++ backup.py
@@ -1,5 +1,8 @@
 import pyodbc
 import const
+import os
+import socket
+import glob
 
 DRIVER_NAME = const.DRIVER_NAME
 SERVER_NAME = const.SERVER_NAME
@@ -7,41 +10,81 @@ UID = const.UID
 PASSWORD = const.PASSWORD
 WINAUTH = const.USE_WIN_AUTHENTICATION
 WHERECONDITION = const.WHERECONDITION
-BACKUP_FOLDER = const.BACKUP_FOLDER
+BACKUP_FOLDER = const.BACKUP_FOLDER + os.sep
 
-if WINAUTH:
-    conn = pyodbc.connect(
-        f"Driver={DRIVER_NAME};Server={SERVER_NAME};Trusted_Connection=yes;"
-    )
-else:
-    conn = pyodbc.connect(
-        f"Driver={DRIVER_NAME};Server={SERVER_NAME};UID={UID};PWD={PASSWORD};"
-    )
 
-sqlCommand = f"""DECLARE @name VARCHAR ( 50 ) -- database name 
-                DECLARE @path VARCHAR ( 256 ) -- path for backup files 
-                DECLARE @fileName VARCHAR ( 256 ) -- filename for backup 
-                DECLARE @fileDate VARCHAR ( 20 ) -- used for file name 
+def backup():
+    # first, i need to check if SERVER_NAME is the current machine, if it is not, i will not do anything
+    # if (
+    #    socket.gethostname() != SERVER_NAME
+    #    and socket.gethostbyname(socket.gethostname()) != SERVER_NAME
+    # ):
+    #    print(
+    #        "You are trying to backup from a machine that is not the machine where the instance is installed. You can't do that."
+    #    )
+    #    return None
 
-                SET @path = '{BACKUP_FOLDER}\'
+    if WINAUTH:
+        conn = pyodbc.connect(
+            f"Driver={DRIVER_NAME};Server={SERVER_NAME};Trusted_Connection=yes;"
+        )
+    else:
+        conn = pyodbc.connect(
+            f"Driver={DRIVER_NAME};Server={SERVER_NAME};UID={UID};PWD={PASSWORD};"
+        )
 
-                SELECT @fileDate = CONVERT ( VARCHAR ( 20 ), GETDATE (), 112 ) 
+    sqlCommand = f"""DECLARE @name VARCHAR ( 50 ) -- database name 
+                    DECLARE @path VARCHAR ( 256 ) -- path for backup files 
+                    DECLARE @fileName VARCHAR ( 256 ) -- filename for backup 
+                    DECLARE @fileDate VARCHAR ( 20 ) -- used for file name 
 
-                DECLARE db_cursor CURSOR FOR 
-                SELECT name 
-                FROM master.dbo.sysdatabases 
-                WHERE {WHERECONDITION}
+                    SET @path = '{BACKUP_FOLDER}'
 
-                OPEN db_cursor 
-                FETCH NEXT FROM db_cursor INTO @name 
+                    SELECT @fileDate = CONVERT ( VARCHAR ( 20 ), GETDATE (), 112 ) 
 
-                WHILE @@FETCH_STATUS = 0 
-                BEGIN 
-                SET @fileName = @path + @name + '_' + @fileDate + '.BAK' 
-                BACKUP DATABASE @name TO DISK = @fileName with init 
+                    DECLARE db_cursor CURSOR FOR 
+                    SELECT name 
+                    FROM master.dbo.sysdatabases 
+                    {WHERECONDITION}
 
-                FETCH NEXT FROM db_cursor INTO @name 
-                END 
+                    OPEN db_cursor 
+                    FETCH NEXT FROM db_cursor INTO @name 
 
-                CLOSE db_cursor 
-                DEALLOCATE db_cursor"""
+                    WHILE @@FETCH_STATUS = 0 
+                    BEGIN 
+                    SET @fileName = @path + @name + '_' + @fileDate + '.BAK' 
+                    BACKUP DATABASE @name TO DISK = @fileName with init 
+
+                    FETCH NEXT FROM db_cursor INTO @name 
+                    END 
+
+                    CLOSE db_cursor 
+                    DEALLOCATE db_cursor"""
+
+    # fisrt i will check if the directory exists, if it doesn't I will create it
+    os.makedirs(BACKUP_FOLDER, exist_ok=True)
+
+    # now i will execute the sql command
+    conn.autocommit = True
+    cursor = conn.cursor()
+    print("Starting the backup...")
+    cursor.execute(sqlCommand).commit()
+    print("Backup done!")
+
+    # now i will check all the names of the different databases
+    backup_files = glob.glob(BACKUP_FOLDER + "*_????????.bak")
+    # with os.listdir(BACKUP_FOLDER) as entries:
+    #    print("im in 1")
+    #    for entry in entries:
+    #        print("im in 2")
+    #        if entry.is_file() and re.match("*_????????.bak", entry.name):
+    #            print("im in 3")
+    #            backup_files.append(entry.name)
+    for name in backup_files:
+        print(name)
+    # now i will check if the last backup done, for each database, has the sime size as the previous one, if it does I will delete the last one
+
+    # now, considering hom many copies of each db I have to keep I will delete the oldest ones
+
+
+backup()
--- const.py
+++ const.py
@@ -1,8 +1,9 @@
 # DRIVER_NAME = "{/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.2.1}"
-DRIVER_NAME = "{ODBC Driver 17 for SQL Server}"
-SERVER_NAME = "localhost"
+# DRIVER_NAME = "{ODBC Driver 18 for SQL Server}"
+DRIVER_NAME = "{SQL Server}"
+SERVER_NAME = "10.0.0.179"
 USE_WIN_AUTHENTICATION = True
 UID = "backupsa"
 PASSWORD = "ImtheonedoingtheBackup"
-WHERECONDITION = "WHERE [name] LIKE 'ADB%' AND [name] NOT IN ('ADB_SYSTEM', 'ADB_DEMO')"
-BACKUP_FOLDER = "/home/backupuser/backup/"
+WHERECONDITION = "WHERE [name] LIKE 'ADB%' AND [name] NOT IN ('ADB_SYSTEM', 'ADB_DEMO') and [name] = 'ADB_GELA'"
+BACKUP_FOLDER = "C:\OSRA\COPIE"
